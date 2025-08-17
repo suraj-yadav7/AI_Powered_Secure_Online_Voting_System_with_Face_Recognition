8a9694a88d2936b440a8468d287fc6723f2832fe
suraj <alphaguy433@gmail.com>
2025-08-17 21:45:09 +0530

    Added only backend files for deployment on render.

--- client/src/utils/endpointUrls.js
+++
@@ -1,8 +0,0 @@
-const base_url = import.meta.env.VITE_BASE_URL
-
-export const api = {
-  login:`${base_url}/auth/login`,
-  signup:`${base_url}/auth/register`,
-  voter_register:`${base_url}/voter/register`,
-  generic_fetch:`${base_url}/generic/fetch`
-};
